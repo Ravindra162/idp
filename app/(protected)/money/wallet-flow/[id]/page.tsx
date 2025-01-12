6b4caa1d45c6cf8eef98104ca6eaca852a839500
DHRUVA-N-L-CHOUDHARY <lci2021014@iiitl.ac.in>
2025-01-12 19:06:27 +0530

    wallet_flow filter

--- app/(protected)/money/wallet-flow/[id]/page.tsx
+++ app/(protected)/money/wallet-flow/[id]/page.tsx
@@ -33,7 +33,7 @@ const WalletFlow = async ({
   const totalPages = Math.ceil(totalItemCount / pageSize);
 
   const walletFlow = await db.walletFlow.findMany({
-    where: { userId: params.id },
+    where: { userId: params.id, status: "SUCCESS" },
     take: pageSize,
     skip: (currentPage - 1) * pageSize,
     orderBy: { createdAt: "desc" },
