5e6763a7c8deac4bb1982636d1d4fb9ffb4e4c41
DHRUVA-N-L-CHOUDHARY <lci2021014@iiitl.ac.in>
2025-05-12 04:29:47 +0000

    balance card

--- app/(protected)/_components/BalanceCard.tsx
+++ app/(protected)/_components/BalanceCard.tsx
@@ -1,24 +1,45 @@
-import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import {auth} from "@/auth";
 import React from "react";
-import TotalMoney from "./TotalMoney";
+import {db} from "@/lib/db";
+import {formatPrice} from "@/components/shared/formatPrice";
 
 const BalanceCard = async () => {
-  const session = await auth();
-  return (
-    <div className="flex justify-around flex-col mx-2 mt-5 w-full md:w-56 bg-gray-100 p-2 rounded-lg h-24 md:h-28">
-      <div className="flex justify-between items-center">
-        <span className="font-semibold">Balance</span>
-        <Button variant={"link"} className="font-medium" asChild>
-          <Link href={`/money/record/${session?.user?.id}`}>Add money</Link>
-        </Button>
-      </div>
-      <p className="mt-3">
-        <TotalMoney />
-      </p>
-    </div>
-  );
+    const session = await auth();
+    const wallets = await db.wallet.findMany({
+        where: {
+            userId: session?.user.id,
+        },
+        include: {
+            walletType: {
+                select: {
+                    currencyCode: true,
+                },
+            },
+        },
+    });
+
+    const walletDetails = wallets
+        .filter(wallet => wallet.walletType)
+        .map(wallet => ({
+            balance: wallet.balance,
+            currencyCode: wallet.walletType.currencyCode,
+        }));
+
+    return (
+        <div className="space-y-4">
+            {walletDetails.map((wallet, idx) => (
+                <div
+                    key={idx}
+                    className="flex justify-around flex-col mx-2 mt-2 w-full md:w-56 bg-gray-100 p-2 rounded-lg h-24 md:h-28"
+                >
+                    <div className="flex justify-between items-center">
+                        <span className="font-semibold">Balance ({wallet.currencyCode})</span>
+                    </div>
+                    <p className="mt-3">{formatPrice(wallet.balance, wallet.currencyCode)}</p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default BalanceCard;
